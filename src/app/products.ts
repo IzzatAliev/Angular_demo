7cca7632926f3ac7afb304831e024bb03e146d83
IzzatAliev <alibax1515@gmail.com>
2021-11-18 23:55:28 +0200

    angular_demo

---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens' 
+    },
+    {
+        name: 'Phone XS',
+        price: 900,
+        description: 'A large phone with one of the best cameras'   
+    },
+    {
+        name: 'Phone X mini',
+        price: 699,
+        description: 'A good phone for this price'   
+    }
+];
