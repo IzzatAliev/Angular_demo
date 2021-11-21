d33ebb30c51bbebdf127995d0a773d902ffe8465
IzzatAliev <alibax1515@gmail.com>
2021-11-21 18:20:01 +0200

    demoProductApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,35 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { MatSliderModule } from '@angular/material/slider'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     TopBarComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
+    MatSliderModule,
     BrowserModule,
     AppRoutingModule,
     CommonModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { 
-        path: '', 
-        component: ProductListComponent
-      },
-    ])
+      { path: '',component: ProductListComponent},
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products.ts
+++ src/app/products.ts
@@ -13,5 +13,30 @@ export const products = [
         name: 'Phone X mini',
         price: 699,
         description: 'A good phone for this price'   
+    },
+    {
+        name: 'Xiaomi 8',
+        price: 650,
+        description: 'A good phone for this price'   
+    },
+    {
+        name: 'Xiaomi 8 pro',
+        price: 700,
+        description: 'A good phone for this price'   
+    },
+    {
+        name: 'Xiaomi 7',
+        price: 699,
+        description: 'A good phone for this price'   
+    },
+    {
+        name: 'Xiaomi 9',
+        price: 678,
+        description: 'A good phone for this price'   
+    },
+    {
+        name: 'Xiaomi 9 pro',
+        price: 760,
+        description: 'A good phone for this price'   
     }
 ];
